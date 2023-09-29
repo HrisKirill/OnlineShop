ab09c69239e25041ccb71cac6d12f73db16a777a
HrisKirill <hristoforow.kirill@gmail.com>
2023-09-29 14:22:51 +0300

    add filling db with starting =

--- src/main/java/com/khrystoforov/onlineshopapp/OnlineShopAppApplication.java
+++ src/main/java/com/khrystoforov/onlineshopapp/OnlineShopAppApplication.java
@@ -1,18 +1,13 @@
 package com.khrystoforov.onlineshopapp;
 
-import com.khrystoforov.onlineshopapp.entity.Order;
 import com.khrystoforov.onlineshopapp.entity.Product;
-import com.khrystoforov.onlineshopapp.entity.enums.OrderStatus;
-import com.khrystoforov.onlineshopapp.service.OrderService;
+import com.khrystoforov.onlineshopapp.entity.enums.ProductStatus;
 import com.khrystoforov.onlineshopapp.service.ProductService;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.scheduling.annotation.EnableScheduling;
-
-import java.time.LocalDateTime;
-import java.util.ArrayList;
 
 @SpringBootApplication
 @EnableScheduling
@@ -23,23 +18,19 @@ public class OnlineShopAppApplication {
     }
 
     @Bean
-    CommandLineRunner runner(ProductService productService,
-                             OrderService orderService) {
+    CommandLineRunner runner(ProductService productService) {
         return args -> {
-            productService.create(new Product(1L, "IPhone 13 Pro", 1000.));
-            productService.create(new Product(2L, "IPhone 13 Pro", 1000.));
-            productService.create(new Product(3L, "IPhone 13 Pro", 1000.));
-            productService.create(new Product(4L, "IPhone 13 Pro", 1000.));
+            productService.create(new Product(1L, "IPhone 13 Pro", 1000., ProductStatus.FREE));
+            productService.create(new Product(2L, "IPhone 13 Pro", 1000., ProductStatus.FREE));
+            productService.create(new Product(3L, "IPhone 13 Pro", 1000., ProductStatus.FREE));
+            productService.create(new Product(4L, "IPhone 13 Pro", 1000., ProductStatus.FREE));
 
-            productService.create(new Product(5L, "IPhone 11", 850.));
-            productService.create(new Product(6L, "IPhone 11", 850.));
-            productService.create(new Product(7L, "IPhone 11", 850.));
+            productService.create(new Product(5L, "IPhone 11", 850., ProductStatus.FREE));
+            productService.create(new Product(6L, "IPhone 11", 850., ProductStatus.FREE));
+            productService.create(new Product(7L, "IPhone 11", 850., ProductStatus.FREE));
 
-            productService.create(new Product(8L, "IPhone 7 Plus", 500.));
-            productService.create(new Product(9L, "IPhone 7 Plus", 500.));
-
-            orderService.create(new Order(1L, LocalDateTime.now(), OrderStatus.UNPAID, new ArrayList<>()));
-
+            productService.create(new Product(8L, "IPhone 7 Plus", 500., ProductStatus.FREE));
+            productService.create(new Product(9L, "IPhone 7 Plus", 500., ProductStatus.FREE));
         };
     }
 }
