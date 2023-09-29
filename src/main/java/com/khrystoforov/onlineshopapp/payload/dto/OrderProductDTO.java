80ed66260fe7ba7e3eec423cad9f5ffc9f5cd1ad
HrisKirill <hristoforow.kirill@gmail.com>
2023-09-29 14:18:22 +0300

    rename field productDTO

--- src/main/java/com/khrystoforov/onlineshopapp/payload/dto/OrderProductDTO.java
+++ src/main/java/com/khrystoforov/onlineshopapp/payload/dto/OrderProductDTO.java
@@ -6,6 +6,6 @@ import lombok.Data;
 @Data
 @AllArgsConstructor
 public class OrderProductDTO {
-    private ProductDTO productDTO;
+    private ProductDTO product;
     private Double totalPrice;
 }
