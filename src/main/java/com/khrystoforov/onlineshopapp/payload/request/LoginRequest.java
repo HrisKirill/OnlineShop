825c3825d1a0c1ce7bc7ba5fda65d77e39fad98f
HrisKirill <hristoforow.kirill@gmail.com>
2023-09-30 18:20:21 +0300

    add builder for requests

--- src/main/java/com/khrystoforov/onlineshopapp/payload/request/LoginRequest.java
+++ src/main/java/com/khrystoforov/onlineshopapp/payload/request/LoginRequest.java
@@ -1,8 +1,10 @@
 package com.khrystoforov.onlineshopapp.payload.request;
 
+import lombok.Builder;
 import lombok.Getter;
 
 @Getter
+@Builder
 public class LoginRequest {
     private String email;
     private String password;
--- src/main/java/com/khrystoforov/onlineshopapp/payload/request/RegisterRequest.java
+++ src/main/java/com/khrystoforov/onlineshopapp/payload/request/RegisterRequest.java
@@ -1,8 +1,11 @@
 package com.khrystoforov.onlineshopapp.payload.request;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
 
 @Getter
+@Builder
 public class RegisterRequest {
     private String email;
     private String username;
