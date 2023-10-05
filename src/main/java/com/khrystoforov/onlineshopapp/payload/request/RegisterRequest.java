23d5655dd5aa633dd920af1dc8fead402d562820
HrisKirill <hristoforow.kirill@gmail.com>
2023-10-05 14:23:27 +0300

    add validation to RegisterRequest

--- src/main/java/com/khrystoforov/onlineshopapp/payload/request/RegisterRequest.java
+++ src/main/java/com/khrystoforov/onlineshopapp/payload/request/RegisterRequest.java
@@ -1,14 +1,25 @@
 package com.khrystoforov.onlineshopapp.payload.request;
 
-import lombok.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
 
 @Builder
 @AllArgsConstructor
 @NoArgsConstructor
 @Data
 public class RegisterRequest {
+    @Email(message = "Email is not valid", regexp = "^[a-zA-Z0-9_!#$%&'*+/=?`{|}~^.-]+@[a-zA-Z0-9.-]+$")
+    @NotBlank(message = "Email cannot be empty")
     private String email;
+    @NotBlank(message = "Name is required.")
     private String username;
+    @NotBlank(message = "Role can not be empty")
     private String role;
+    @NotBlank(message = "Password can not be empty")
     private String password;
 }
